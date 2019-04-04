e12d2bc4cad0ddbe3f31a49b9aa7bc29e33f4c45
Boris Fazio <boris.fazio@outlook.com>
2019-04-03 23:34:21 -0500

    replace hardcoded data path for local config-able one

--- scripts/setup.R
+++ scripts/setup.R
@@ -4,7 +4,7 @@ library(here)
 library(brms)
 options(mc.cores = parallel::detectCores())
 
-DATADIR <- "D:/datasets/inei/endes/"
+DATADIR <- ini::read.ini("config.ini")$local$DATADIR
 
 `%p0%` <- function(x, y){
   paste0(x, y)
