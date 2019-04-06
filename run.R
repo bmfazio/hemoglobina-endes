67cba1acdfc50cc5593f772e60f0ae16e2081bed
Boris Fazio <boris.fazio@outlook.com>
2019-04-06 10:49:17 -0500

    fixed packrat gitignore issue

--- run.R
+++ run.R
@@ -4,6 +4,7 @@ options("stringsAsFactors" = FALSE)
 #options(mc.cores = parallel::detectCores())
 options(mc.cores = 2)
 Sys.setenv(LOCAL_CPPFLAGS = '-march=native')
+packrat::set_opts(vcs.ignore.src = TRUE, vcs.ignore.lib = TRUE)
 DATADIR <- ini::read.ini("config.ini")$local$DATADIR
 
 # Run
